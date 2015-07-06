f1855679a236a02fc4f26ca10cb8f7a43295cfc7
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-07-06 20:58:44 +1000

    detect indents in slim-ish strings

--- lib/slix.ex
+++ lib/slix.ex
@@ -1,2 +1,27 @@
 defmodule Slix do
+  def render(template_string) do
+    String.split(template_string, "\n")
+    |> Enum.map fn(x) -> detect_indent x end
+  end
+
+  def detect_indent(str) do
+    stripped = String.lstrip(str)
+    [ indent: String.length(str) - String.length(stripped) , line: stripped ]
+  end
+
+  def html(tag) do
+    "<#{tag}>"
+  end
+
+  def closing_tag(tag) do
+    html("/#{tag}")
+  end
+
+  def html(tag, content) when is_function(content) do
+    html(tag, content.())
+  end
+
+  def html(tag, content) do
+    "#{html(tag)}#{content}#{closing_tag(tag)}"
+  end
 end
--- test/slix_test.exs
+++ test/slix_test.exs
@@ -1,7 +1,39 @@
 defmodule SlixTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "html tags" do
+    assert Slix.html("br") == "<br>"
+  end
+
+  test "html tags with content" do
+    assert Slix.html("p", "content") == "<p>content</p>"
+  end
+
+  test "html tags with content fn" do
+    f = fn -> "some whatever content" end;
+    assert Slix.html("p", f) == "<p>some whatever content</p>"
+  end
+
+  test "detects indentation" do
+    source = "    p"
+
+    assert Slix.detect_indent(source) == [ indent: 4, line: "p"]
+  end
+
+  test "indentation for multiple lines" do
+    source_lines = """
+    div
+      p Hello
+        a href="#"
+    h3 slim
+    """
+
+    assert Slix.render(source_lines) == [
+      [ indent: 0, line: "div"],
+      [ indent: 2, line: "p Hello"],
+      [ indent: 4, line: ~S{a href="#"}],
+      [ indent: 0, line: "h3 slim"],
+      [ indent: 0, line: ""],
+    ]
   end
 end
