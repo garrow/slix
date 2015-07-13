3b97d52b9bb9427b985c7117327c8780e21e6e4f
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-07-13 21:17:11 +1000

    Just do indentation again.
    * removes all html methods
    * go back to just detecting indentation and grouping lines by indent

--- lib/slix.ex
+++ lib/slix.ex
@@ -8,72 +8,19 @@ defmodule Slix do
     render(template_string) |> group_lines |> render_eex
   end
 
-
-  def reindent(content, 0), do: content
-  def reindent(content, i) do
-     reindent(" #{content}", i-1)
-  end
-
-
-  def render_eex([]), do: ""
-  def render_eex([h | t]) do
-    "#{render_eex(h)}#{render_eex(t)}"
-    #render_eex(h)
-  end
-
-  def render_eex(%{ indent: indent, line: line, nesting: nesting }) do
-
-
-    cond do
-      line  =~ ~r{^$} -> ""
-      match = Regex.run(~r{^('\s?)(.+)$}, line) ->
-        [_, _prefix, text_content] = match
-        r = text_content
-
-
-      true ->
-        r = html(line, render_eex(nesting))
-    end
-
-    reindent(r, indent)
-    #render_slim(line, render_eex(nesting))#, Enum.map(item[:nesting], &render_eex/1 ))
-    #html(item[:line], Enum.map(item[:nesting], &render_eex/1 ))
-  end
-
-
-
+  def detect_indent([]),  do: %{ indent: 0, line: "", nesting: [] }
   def detect_indent(str) do
     stripped = String.lstrip(str)
     %{ indent: String.length(str) - String.length(stripped), line: stripped, nesting: [] }
   end
 
   def group_lines([]), do: []
-  def group_lines([ h | t ]) do
-
-    {nesting, rest} = children_of(h[:indent], t)
-
-    [ %{ h | nesting: group_lines(nesting) } | group_lines(rest)]
-      #cond do
-    #h[:indent] == depth ->
-    # h[:indent] > depth ->
-        #[ h ++ [nesting: group_lines(h[:indent], t)] ]
-      #h[:indent] < depth ->
-        #[ ] # shit, this approach is broken
-        #end
-
-#    cond do
-#      h[:indent] == depth ->
-#        [h | group_lines(h[:indent], t)]
-#      h[:indent] > depth ->
-#        [ h ++ [nesting: group_lines(h[:indent], t)] ]
-#      h[:indent] < depth ->
-#        [ ] # shit, this approach is broken
-#    end
+  def group_lines([ h = %{indent: _, line: _, nesting: _} | t ]) do
+    { nesting, rest } = children_of(h[:indent], t)
+    [ %{ h | nesting: group_lines(nesting) } | group_lines(rest) ]
   end
 
-  def children_of(_, []) do
-    [[],[]]
-  end
+  def children_of(_, []), do: { [],[] }
   def children_of(depth, list) do
     Enum.split_while(list, fn(x) -> x[:indent] > depth end)
   end
--- test/slix_test.exs
+++ test/slix_test.exs
@@ -24,70 +24,21 @@ defmodule SlixTest do
     ]
   end
 
-  test "render JUST MAKE ONE TEST PASS, WHO CARES ABOUT WHITESPACE" do
-    source_lines = """
-    div
-      p
-        a
-          ' LinkText
-    h3
-      ' Text
-    """
+ test "grouping lines with simple indentation" do
+   lines = [
+     %{ indent: 0, line: "div", nesting: []},
+     %{ indent: 2, line: "p Hello", nesting: []},
+     %{ indent: 4, line: ~S{a href="#"}, nesting: []},
+   ]
 
-    assert Slix.render_nested(source_lines) == """
-    <div>
-      <p>
-        <a>
-          LinkText</a>
-    </p>
-    </div>
-    <h3>
-      Text
-    </h3>
-    """
-  end
+   output = [
+     %{ indent: 0, line: "div", nesting: [
+       %{ indent: 2, line: "p Hello", nesting: [
+         %{ indent: 4, line: ~S{a href="#"}, nesting: [ ]} ]} ]},
+   ]
 
-
-  test "render nested" do
-    source_lines = """
-    div
-      p
-        a href="#" LinkText
-    h3
-      ' Text
-    """
-
-    assert Slix.render_nested(source_lines) == """
-    <div>
-      <p>
-        <a href="#">LinkText</a>
-      </p>
-    </div>
-    <h3>
-      Text
-    </h3>
-    """
-  end
-
-
-
-#  test "grouping lines with simple indentation" do
-#    lines = [
-#      %{ indent: 0, line: "div"},
-#      %{ indent: 2, line: "p Hello"},
-#      %{ indent: 4, line: ~S{a href="#"}},
-#    ]
-#
-#    output = [
-#      %{ indent: 0, line: "div", nesting: [
-#        %{ indent: 2, line: "p Hello", nesting: [
-#          %{ indent: 4, line: ~S{a href="#"}, nesting: [ ]} ]} ]},
-#    ]
-#
-#    assert Slix.group_lines(lines) == output
-#
-#  end
-
+   assert Slix.group_lines(lines) == output
+ end
 
   test "children of" do
 
